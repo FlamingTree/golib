4d5641f644cf467517850e19c1c0c0ec136e661a
shaoguang <shaoguang0606@gmail.com>
2015-12-03 20:34:04 +0800

    增加waitquit

---
+++ waitquit/waitquit.go
@@ -0,0 +1,34 @@
+package waitquit
+
+import (
+	"sync"
+)
+
+type WaitQuit struct {
+	*sync.WaitGroup
+	exitChan chan struct{}
+}
+
+func NewWaitQuit() WaitQuit {
+	return WaitQuit{
+		WaitGroup: &sync.WaitGroup{},
+		exitChan:  make(chan struct{}),
+	}
+}
+
+func (wq WaitQuit) Exit() {
+	close(wq.exitChan)
+	wq.Wait()
+}
+
+func (wq WaitQuit) Wrap(cb func()) {
+	wq.Add(1)
+	go func() {
+		cb()
+		wq.Done()
+	}()
+}
+
+func (wq WaitQuit) Done() struct{} {
+	return wq.exitChan
+}
